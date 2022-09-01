6f8f7502e563ff1eb4158c7aa91b6ef94b351060
anredera <sidikriyan@users.noreply.github.com>
2022-09-01 18:05:11 +0700

    Update hello_world_test.go

--- projects/go_hello_world/hello_world_test.go
+++ projects/go_hello_world/hello_world_test.go
@@ -3,7 +3,7 @@ package go_hello_world
 import "testing"
 
 func TestGreeter(t *testing.T) {
-	expected := "hello world!"
+	expected := "hello world new!"
 	actual := HelloWorld()
 	if actual != expected {
 		t.Errorf("expected %q but got %q", expected, actual)
